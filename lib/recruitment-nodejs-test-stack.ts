efe598c971637deb59d106363ebdd8455c1e591e
lavitra8 <59783162+lavitra8@users.noreply.github.com>
2024-12-15 14:57:16 +0530

    Added required packages for SNS and Lambda
    Created an SNS topic called donation

--- lib/recruitment-nodejs-test-stack.ts
+++ lib/recruitment-nodejs-test-stack.ts
@@ -1,9 +1,13 @@
 import * as cdk from '@aws-cdk/core';
+import * as sns from '@aws-cdk/aws-sns';
 
 export class RecruitmentNodejsTestStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create SNS topic
+    const donationTopic = new sns.Topic(this, 'DonationTopic', {
+      displayName: 'User Donation Thank You Topic'
+    });
   }
 }
