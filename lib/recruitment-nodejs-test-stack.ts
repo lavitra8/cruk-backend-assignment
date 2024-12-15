7554be12fd5f4e16e5a6206ca8eeee82176f60fc
lavitra8 <59783162+lavitra8@users.noreply.github.com>
2024-12-15 15:21:01 +0530

    Added API gateway and added Lambda function to it

--- lib/recruitment-nodejs-test-stack.ts
+++ lib/recruitment-nodejs-test-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from '@aws-cdk/core';
 import * as sns from '@aws-cdk/aws-sns';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as snsSubscriptions from '@aws-cdk/aws-sns-subscriptions';
+import * as apigateway from '@aws-cdk/aws-apigateway';
 
 export class RecruitmentNodejsTestStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -28,5 +29,17 @@ export class RecruitmentNodejsTestStack extends cdk.Stack {
     // Subscribe an email to the topic
     donationTopic.addSubscription(new snsSubscriptions.EmailSubscription('contactsubodhkharat@gmail.com'));
 
+    // Create API Gateway
+    const api = new apigateway.RestApi(this, 'donationApi', {
+      restApiName: 'Donation Service',
+      description: 'This service handles donations.'
+    });
+
+    // Create API Gateway Lambda integration
+    const donationIntegration = new apigateway.LambdaIntegration(donationFunction);
+
+    // Create API Gateway resource and method
+    const donations = api.root.addResource('donations');
+    donations.addMethod('POST', donationIntegration);
   }
 }
